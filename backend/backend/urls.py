f80331dbac363ce77f5b008dbfe5bcbca6b77ee5
ka-few <njorovista@gmail.com>
2025-09-05 10:30:13 +0300

    Fix urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+def health_check(request):
+    return HttpResponse("Backend running 🚀", content_type="text/plain")
+
+
 urlpatterns = [
+    path("", health_check),
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
