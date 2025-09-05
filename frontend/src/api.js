a9b7f1a6fb3b24897c1f81b37b7d4e553e515960
ka-few <njorovista@gmail.com>
2025-09-05 20:02:35 +0300

    connect backend to frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,11 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiUrl = "https://20884de5-c0f0-4731-992e-0b339b2d4aa9-dev.e1-us-east-azure.choreoapis.dev/notes-app/backend/v1"
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL 
-    // ? import.meta.env.VITE_API_URL : apiUrl
+    ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 api.interceptors.request.use(
